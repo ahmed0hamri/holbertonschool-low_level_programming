3585c61ebb175dd875406e4293248dc6f3089ad9
ahmed0hamri <7mid7imri@gmail.com>
2023-10-19 07:14:09 -0700

    script 4

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+*main - Entry point
+*
+*a program that prints the alphabet in lowercase.
+*
+*Return: Always (Success)
+*/
+int main(void)
+{
+char ch;
+
+for (ch = 'a'; ch <= 'z' ; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+
+ if alpha_letters == 'e' or alpha_letters == 'q':
+       continue
+return (0);
+}
+
