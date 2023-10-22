fa8a8ad452eee2bd62b5bbeda96fb6ecb086e5c3
ahmed0hamri <7mid7imri@gmail.com>
2023-10-22 15:33:13 -0700

    script 8

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_square - print a square
+ * @size: integer
+ * Return: void (Success) .
+ */
+
+void print_square(int size)
+{
+	int a;
+	int b;
+
+	if (size > 0)
+	{
+		for (a = 0; a < size; a++)
+		{
+			for (b = 0; b < size; b++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
