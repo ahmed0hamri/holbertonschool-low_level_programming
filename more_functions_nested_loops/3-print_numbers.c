9ca0d85b8250658e47f09d7f292934274afbe53d
ahmed0hamri <7mid7imri@gmail.com>
2023-10-22 15:26:57 -0700

    script 3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_numbers - check for a digit
+ * function that checks for numbers
+ * Return: Always 0.
+ */
+
+void print_numbers(void)
+{
+
+int c;
+
+for (c = 0; c <= 9; c++)
+
+{
+_putchar('0' + c);
+}
+
+_putchar('\n');
+
+}
