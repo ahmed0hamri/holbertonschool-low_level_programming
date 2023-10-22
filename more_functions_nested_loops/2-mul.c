c40da6670879e2927cbc4a90f5ae79f80e8b0699
ahmed0hamri <7mid7imri@gmail.com>
2023-10-22 15:26:07 -0700

    script 2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * mul - check for a digit
+ *@a : integer
+ *@b : integer
+ * Return: Always 0.
+ */
+
+int mul(int a, int b)
+{
+
+{
+return (a * b);
+}
+
+return (0);
+
+}
