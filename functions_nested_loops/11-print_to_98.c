dc424a86633a81fc5d0a8dceadf936ea1deefce1
ahmed0hamri <7mid7imri@gmail.com>
2023-10-22 13:33:15 -0700

    script 11

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,36 +1,43 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_to_98 - check the code.
- *@n: int
- * Return: Always 0.
+ * print_to_98 - Check description
+ * Description: It prints 9 times table starting with 0
+ * @n : integer
+ * Return: Nothing.
  */
+
 void print_to_98(int n)
 {
 int i;
 
-if (n <= 98)
+if (n >= 98)
 {
-for (i = n ; i <= 98 ; i++)
+for (i = n; i >= 98; i--)
 {
-printf("%d", i);
 if (i != 98)
 {
-printf(", ");
+printf("%d, ", i);
+}
+else
+{
+printf("%d", i);
+}
+}
+}
+else
+{
+for (i = n; i <= 98; i++)
+{
+if (i != 98)
+{
+printf("%d, ", i);
+}
+else
+{
+printf("%d", i);
+}
 }
 }
 printf("\n");
-}
-if (n > 98)
-{
-for (i = n ; i >= 98 ; i--)
-{
-printf("%d", i);
-if (i != 98)
-{
-printf(", ");
-}
-}
-printf("\n");
-}
 }
