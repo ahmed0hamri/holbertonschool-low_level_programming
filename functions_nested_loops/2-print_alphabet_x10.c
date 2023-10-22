9313749900d47215ddfc6c58345bc4d1ebabbd45
ahmed0hamri <7mid7imri@gmail.com>
2023-10-22 13:02:33 -0700

    script 2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - check the code.
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		char ch = 'a';
+
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+		_putchar('\n');
+	}
+}
