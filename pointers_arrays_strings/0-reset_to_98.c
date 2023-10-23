077a99f672affcd26259695c0daafe1a6a6cf140
ahmed0hamri <7mid7imri@gmail.com>
2023-10-23 04:50:58 -0700

    script 0

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -2,16 +2,13 @@
 #include <stdio.h>
 
 /**
- * main - check the code 
- *
- * Return: Always 0.
- */
-int void reset_to_98(int *n);
-{
-    int n;
+  * reset_to_98 - Updates the value it points to 98
+  * @n: pointer to n
+  *
+  * Return: void
+  */
 
-    n = 98;
-    printf("n=%d\n", n);
-    reset_to_98(&n);
-    printf("n=%d\n", n);
-    return (0);
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
