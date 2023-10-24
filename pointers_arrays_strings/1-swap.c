0ab6afdac91470b561ad5fad20c0cc88c8062a26
ahmed0hamri <7mid7imri@gmail.com>
2023-10-24 01:35:20 -0700

    script 1

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * swap_int - check the code
+ *@a: pointer
+ *@b: pointer
+ * Return: Always (Success)
+ */
+void swap_int(int *a, int *b)
+{
+	int n;
+
+	*a = n;
+	*b = *a;
+	n = *b;
+}
