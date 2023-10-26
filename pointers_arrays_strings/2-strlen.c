d19333c41b956b2a746490e359989c135a9bca6b
ahmed0hamri <7mid7imri@gmail.com>
2023-10-26 12:55:44 -0700

    script2

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int _strlen(char *s);{
+	char Chaine[]="bonjour";
+	int length=strlen(Chaine);
+	printf("the lenght of the string = %d",length);
+	return 0;}
