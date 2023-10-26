710268d4edeb6386539941a167a017bf45f8981b
ahmed0hamri <7mid7imri@gmail.com>
2023-10-26 13:28:24 -0700

    script 3

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * * _puts - check the code
+ * @str: pointer
+ * Return: Always 0.
+ */
+void _puts(char *str);<
+{
+
+	for (; *str != '\0'; str++)
+	{
+		_putchar(*str + 0);
+	}
+	_putchar('\n');
+}
