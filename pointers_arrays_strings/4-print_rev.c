cc49dfea7d3752b9487c11e96bc44eaea1d28a1f
ahmed0hamri <7mid7imri@gmail.com>
2023-10-27 03:20:01 -0700

    script5

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_rev(char *s)
+{
+	char str[5];
+	printf (" \n Enter a string to be reversed: ");
+}
