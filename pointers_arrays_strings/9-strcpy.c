2e558b6fc22adb46ed94b0f9ec205195f6ee028f
ahmed0hamri <7mid7imri@gmail.com>
2023-10-29 11:46:37 -0700

    script 9

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * print_array - check the code
+ * @a: pointer
+ * @n: int
+ * Return: Always 0
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", *(a + i));
+		if (i < n - 1)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
