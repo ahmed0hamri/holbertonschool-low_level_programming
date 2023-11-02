7a55a555aa82d3ed798814ef26cc545b3cf36ce1
ahmed0hamri <7mid7imri@gmail.com>
2023-11-02 02:45:18 -0700

    script 0

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -7,7 +7,7 @@
  */
 void _puts_recursion(char *s)
 {
-	if ( *s == '\0')
+	if (*s == '\0')
 	{
 		_putchar('\n');
 	}
@@ -17,6 +17,3 @@ void _puts_recursion(char *s)
 		_puts_recursion(s + 1);
 	}
 }
-
-
-	
