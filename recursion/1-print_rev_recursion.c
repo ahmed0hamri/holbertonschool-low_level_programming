d9d95a767931539a74d615ffeee511472200cb7b
ahmed0hamri <7mid7imri@gmail.com>
2023-11-02 03:05:52 -0700

    script 1

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - check the code
+ * Description: print a string backwards
+ * @s : pointer to string
+ * Return: Void
+ */
+
+void _print_rev_recursion(char *s)
+{
+	
+	if (*s != '\0')
+	{
+		_puts_recursion(s + 1);
+		_putchar(*s);
+	}
+}
