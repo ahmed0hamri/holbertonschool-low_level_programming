ff314a1ecc1872f1e0029545732f74ee56748501
ahmed0hamri <7mid7imri@gmail.com>
2023-11-02 04:21:33 -0700

    script 2

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -1,15 +1,17 @@
 #include "main.h"
-
-
-
-
+/**
+ * _strlen_recursion - check the code
+ * Description: count the length of a string
+ * @s : pointer to string
+ * Return: Void
+ */
 
 int _strlen_recursion(char *s)
 {
 
 	if (!*s)
 		return(0);
-	else 
-		return (1 +_strlen_recursion(s + 1));
+	else
+		return (1 + _strlen_recursion(s + 1));
 
 }
