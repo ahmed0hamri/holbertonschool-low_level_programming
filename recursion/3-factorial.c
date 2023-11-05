c00993c7abe3844d101a268a836a70e9f1f36e4c
ahmed0hamri <7mid7imri@gmail.com>
2023-11-05 04:34:22 -0800

    script 3

---
+++ recursion/3-factorial.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *factorial - chech the code.
+ *@n: int
+ *Return: Always 0.
+ */
+int factorial(int n);
+	{
+		if (n<0)
+			return (-1);
+		else if (n == 1)
+		{
+			return (1);
+		}
+		return (n * facotrial(n-1));
+	}
