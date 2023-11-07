310b5ee67a48133ec711894a02be201e2ab22dcd
ahmed0hamri <7mid7imri@gmail.com>
2023-11-07 01:29:45 -0800

    script 0

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -20,7 +20,7 @@ char *create_array(unsigned int size, char c)
 		p = malloc(sizeof(char) * size);
 	}
 	while (p == NULL);
-	for (i=0, i<n, i++)
+	for (i=0; i<size; i++)
 		p[i]=c;	
 	return (p);
 }
