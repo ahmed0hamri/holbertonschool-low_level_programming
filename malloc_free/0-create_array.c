e357875488a77884cf8d7e9bf563ae2546466ffa
ahmed0hamri <7mid7imri@gmail.com>
2023-11-07 01:14:04 -0800

    script 0

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -1,2 +1,17 @@
-hell²:wq
+#include"main.h"
 
+char *create_array(unsigned int size, char c);
+char *p
+	n=10;
+{
+	if (size == NULL)
+		return NULL
+}
+{
+	do (*p = malloc size of char *n)
+		while (p == NULL)
+}
+{
+	for (i=0, i<n, i++)
+		return (p)
+}
