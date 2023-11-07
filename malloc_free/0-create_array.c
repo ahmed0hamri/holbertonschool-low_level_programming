de78526491584867df10e06debb4da5d300b3866
ahmed0hamri <7mid7imri@gmail.com>
2023-11-07 01:16:24 -0800

    script 0

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -1,5 +1,12 @@
 #include"main.h"
 
+/**
+ * create_array - initialize an array with n number of cases
+ *@size: number of cases
+ *@c: char
+ * Return: ptr.
+ */
+
 char *create_array(unsigned int size, char c);
 char *p
 	n=10;
