825a3142fd4445b206952824bd620b01083cd524
azizios <hdriaziz699@gmail.com>
2023-11-09 19:03:36 +0100

    strconcat

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,18 @@
+#include"main.h"
+/**
+ * heck the script 
+ * @str: pointer
+ * Return: NULL or string
+ */
+char *str_concat(char *s1, char *s2)
+{
+    char *p;
+    if (s1==NULL)
+        s1="\0";
+    if (s2==NULL)
+        s2="\0";
+    while(p==NULL)
+        p=malloc(strlen(s1)+strlen(s2)+1);
+    p=strcat(s1,s2);
+    return(p);
+}
