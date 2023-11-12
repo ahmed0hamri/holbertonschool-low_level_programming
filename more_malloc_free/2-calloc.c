e9a82468569a15fdaf561d58d535b172a094a2ce
ahmed0hamri <7mid7imri.com>
2023-11-12 14:20:01 -0800

    script 2

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -1,24 +1,25 @@
 #include "main.h"
+
 /**
-* _calloc - function that allocates memory for an array, using malloc.
-* @nmemb: unsigned int.
-* @size: unsigned int.
-* Return: ch
-**/ 
+ * _calloc - function that allocates memory for an array, using malloc.
+ * @nmemb: unsigned int.
+ * @size: unsigned int.
+ * Return: ch
+ **/
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-unsigned int i = 0;
-char *ch;
+	unsigned int i = 0;
+	char *ch;
 
 	if (nmemb == 0 || size == 0)
-	return (NULL);
-		ch = malloc(nmemb * size);
+		return (NULL);
+	ch = malloc(nmemb * size);
 	if (ch == NULL)
-	return (NULL);
-		while (i < nmemb * size)
-		{
+		return (NULL);
+	while (i < nmemb * size)
+	{
 		ch[i] = 0;
 		i++;
-		}
+	}
 	return (ch);
 }
