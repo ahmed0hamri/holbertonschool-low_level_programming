ac6e0dcf18a1945b69d343d0a1c880babcc6725f
ahmed0hamri <7mid7imri.com>
2023-11-12 12:55:28 -0800

    script 3

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,44 @@
+#include <main.h>
+/**
+ * **alloc_grid - returns a pointer to a 2D array of ints
+ * @width: width int
+ * @height: height int
+ * Return: pointer to array
+ */
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+	int **array;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+	array = malloc(sizeof(int *) * height);
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+		array[i] = malloc(sizeof(int) * width);
+		if (array[i] == NULL)
+		{
+			while (i >= 0)
+			{
+				free(array[i]);
+				i--;
+			}
+			free(array);
+			return (NULL);
+		}
+	}
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			array[i][j] = 0;
+		}
+	}
+	return (array);
+}
