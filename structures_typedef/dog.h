0b84379bfa8759018284862611f1b50fbda9505f
ahmed0hamri <7mid7imri.com>
2023-11-16 06:05:39 -0800

    my dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -13,3 +13,8 @@ struct dog
 	float age;
 	char *owner;
 };
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+char *_strcpy(char *dest, char *src);
+void free_dog(dog_t *d);
