45485014e878605913e0e878cb7a87970b118be7
ahmed0hamri <7mid7imri.com>
2023-11-16 09:02:57 -0800

    script 1

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,6 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "dog.h"
 /**
  * init_dog - initialize a variable type "struct dog"
@@ -9,16 +6,12 @@
  * @age: float
  * @owner: char*
  */
-struct dog {
-    char *name;
-    float age;
-    char *owner;
-};
-
-void init_dog(struct dog *d, char *name, float age, char *owner) 
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-    d->name = name;
-    d->age = age;
-    d->owner = owner;
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
-
