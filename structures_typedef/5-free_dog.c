25b46a4628625733e07c0ec7e7f771e80aa72467
ahmed0hamri <7mid7imri.com>
2023-11-16 12:36:06 -0800

    script5

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -7,11 +7,11 @@
  */
 void free_dog(dog_t *d)
 {
-    if (d)
-    { 
-        if (d->name)
-        free(d->owner);
-        if (d->owner)
-        free(d->owner);
-    }
+	if (d)
+	{
+		if (d->name)
+			free(d->owner);
+		if (d->owner)
+			free(d->owner);
+	}
 }
