2b485e9d16970397874fef1af271084589c1fa57
ahmed0hamri <7mid7imri.com>
2023-11-16 12:03:13 -0800

    script2

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -2,8 +2,10 @@
 #include <stdlib.h>
 #include <string.h>
 #include "dog.h"
-void print_dog(struct dog *d);{
-if(d){
+void print_dog(struct dog *d);
+{
+if (d) 
+{
     printf("name: %s\n", d->name ? d->name : "(nil)")
     printf("age: %f\n", d->age)
     printf("owner: %s\n", d->owner ? d->owner : "(nil)")
