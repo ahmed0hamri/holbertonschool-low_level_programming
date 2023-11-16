4481c6aae166e8b5923747800d6cd3f569ecbe09
ahmed0hamri <7mid7imri.com>
2023-11-16 05:33:51 -0800

    my dog

---
+++ structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+/**
+ * struct dog - creating a dog struct
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
