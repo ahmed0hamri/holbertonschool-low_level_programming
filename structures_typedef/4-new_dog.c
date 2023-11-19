dd7e83daf4cd5eef1363cb9a1a05bcddb7175f13
ahmed0hamri <7mid7imri.com>
2023-11-19 12:20:14 -0800

    script4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,2 +1,31 @@
 #include "dog.h"
 dog_t *new_dog(char *name, float age, char *owner);
+{
+	dog_t *d;
+	int n1, n2;
+
+	d = malloc(sizeof(dog_t));
+	if (d == NULL)
+	{
+		return (NULL);
+	}
+	n1 = strlen(name);
+	d->name = malloc(sizeof(char) * (n1 + 1));
+	if (d->name == NULL)
+	{
+		free(d);
+		return (NULL);
+	}
+	n2 = strlen(owner);
+	d->owner = malloc(sizeof(char) * (n2 + 1));
+	if (d->owner == NULL)
+	{
+		free(d->name);
+		free(d);
+		return (NULL);
+	}
+	strcpy(d->name, name);
+	d->age = age;
+	strcpy(d->owner, owner);
+	return (d);
+}
