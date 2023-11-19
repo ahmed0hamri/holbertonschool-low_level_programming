5c7707f76942cd1f93f934d44da62433c9d79835
ahmed0hamri <7mid7imri.com>
2023-11-19 12:32:01 -0800

    script4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,4 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "dog.h"
+/**
+ * new_dog - create a new dog
+ * @name: char *
+ * @age: float
+ * @owner: char *
+ * Return: dog_t *
+ */
 dog_t *new_dog(char *name, float age, char *owner);
 {
 	dog_t *d;
