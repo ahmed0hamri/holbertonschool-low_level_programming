07c2ce30f642fab53932bf34f7b4f9dab2d1f411
ahmed0hamri <7mid7imri.com>
2023-11-19 12:29:02 -0800

    script2

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -1,22 +1,24 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "dog.h"
 /**
- * print_dog - print a "struct dog"
- * @d: struct dog *
- * Return: void
+ * print_dog - Short description
+ * @d: the dog to init
+ * Return: void.
  */
-void print_dog(struct dog *d) if (d != NULL)
+void print_dog(struct dog *d)
 {
+	if (d == NULL)
+	{
+		return;
+	}
 	if (d->name == NULL)
-		printf("Name: (nil)\n");
-	else
-		printf("Name: %s\n", d->name);
-	printf("Age: %f\n", d->age);
+	{
+		d->name = "(nil)";
+	}
 	if (d->owner == NULL)
-		printf("Owner: (nil)\n");
-	else
-		printf("Owner: %s\n", d->owner);
-}
+	{
+		d->owner = "(nil)";
+	}
+	printf("Name: %s\n", d->name);
+	printf("Age: %f\n", d->age);
+	printf("Owner: %s\n", d->owner);
 }
