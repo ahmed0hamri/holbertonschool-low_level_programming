670b0a1792b143cb3e686f7bff30cd03ad3a92b7
ahmed0hamri <7mid7imri.com>
2023-11-19 12:41:37 -0800

    script4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,33 +1,30 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "dog.h"
 /**
- * new_dog - create a new dog
- * @name: char *
- * @age: float
- * @owner: char *
- * Return: dog_t *
+ * *new_dog - check the code
+ * @name : char
+ * @age : float
+ * @owner : char
+ * Return: Always 0.
  */
-dog_t *new_dog(char *name, float age, char *owner);
+dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d;
-	int n1, n2;
+	int l1, l2;
 
 	d = malloc(sizeof(dog_t));
 	if (d == NULL)
 	{
 		return (NULL);
 	}
-	n1 = strlen(name);
-	d->name = malloc(sizeof(char) * (n1 + 1));
+	l1 = strlen(name);
+	d->name = malloc(sizeof(char) * (l1 + 1));
 	if (d->name == NULL)
 	{
 		free(d);
 		return (NULL);
 	}
-	n2 = strlen(owner);
-	d->owner = malloc(sizeof(char) * (n2 + 1));
+	l2 = strlen(owner);
+	d->owner = malloc(sizeof(char) * (l2 + 1));
 	if (d->owner == NULL)
 	{
 		free(d->name);
