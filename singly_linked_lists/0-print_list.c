52fbb279cf589bb149e11ec2f23cf42ca95ecd71
ahmed0hamri <7mid7imri.com>
2023-12-04 13:26:50 -0800

    printing

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+size_t print_list(const list_t *h);
+{
+    if (head == NULL)
+        printf("linked list is empty");
+    struct node *ptr = NULL;
+    ptr = head;
+    while (ptr != NULL)
+    {
+        print("%d", ptr->data);
+        ptr = ptr->link;
+    }
+}
