19b46e99244d1b15aacc7b39f94c0dfcb41b1210
ahmed0hamri <7mid7imri.com>
2023-12-04 13:45:40 -0800

    printing

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -3,11 +3,10 @@
 #include <stdio.h>
 #include "lists.h"
 
-size_t print_list(const list_t *h);
+size_t print_list(const list_t *h)
 {
     int i = 0;
     if (h == NULL)
-        ;
     printf("[0] (nil)\n");
 
     while (h != NULL)
@@ -15,7 +14,7 @@ size_t print_list(const list_t *h);
     {
         print("[%d] %s\n", h->len, h->str);
         ptr = ptr->link;
-        i +
+        i++
             h = h->next;
     }
     return (i)
