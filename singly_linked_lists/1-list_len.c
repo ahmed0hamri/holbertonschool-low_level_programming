20bef954505415e423bb24e1e0948dac2a5cb883
ahmed0hamri <7mid7imri@gmail.com>
2023-12-07 12:53:38 +0100

    script 1

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * list_len -print linked list
+ * @h: string
+ * Return: i
+ */
+size_t list_len(const list_t *h)
+{
+    int i = 0;
+
+    if (!h)
+    {
+        return (0);
+    }
+
+    while (h)
+    {
+
+        h = h->next;
+        i++;
+    }
+    return (i);
+}
