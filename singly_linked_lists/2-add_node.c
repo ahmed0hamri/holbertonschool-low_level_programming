67de4ee034f40c89f0856f149ed6c39a5362a98e
ahmed0hamri <7mid7imri@gmail.com>
2023-12-07 13:58:58 +0100

    script 2

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * add_node -print linked list
+ * @head: string
+ * @str: char
+ * Return: node
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+    int len = 0;
+
+    list_t *new_node = malloc(sizeof(list_t));
+    if (!new_node)
+        return (NULL);
+    new_node->str = strdup(str);
+    len = strlen(new_node->str);
+    new_node->len = len;
+    new_node->next = *head;
+    *head = new_node;
+    return (new_node);
+}
