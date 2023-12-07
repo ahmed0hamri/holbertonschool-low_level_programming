f78830bceef1ee7bc3339955f9604d006d991955
ahmed0hamri <7mid7imri@gmail.com>
2023-12-07 14:05:11 +0100

    script 2

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -7,23 +7,23 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-    int len = 0;
-    list_t *new_node = malloc(sizeof(list_t));
+	int len = 0;
+	list_t *new_node = malloc(sizeof(list_t));
 
-    if (!new_node)
-    {
-        free(new_node);
-        return (NULL);
-    }
-    new_node->str = strdup(str);
-    if (!new_node->str)
-    {
-        free(new_node);
-        return (NULL);
-    }
-    len = strlen(new_node->str);
-    new_node->len = len;
-    new_node->next = *head;
-    *head = new_node;
-    return (new_node);
+	if (!new_node)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->str = strdup(str);
+	if (!new_node->str)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	len = strlen(new_node->str);
+	new_node->len = len;
+	new_node->next = *head;
+	*head = new_node;
+	return (new_node);
 }
