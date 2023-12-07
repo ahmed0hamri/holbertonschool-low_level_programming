a4bf11cce29be5f7a84a37c4dc85516bcb09143e
ahmed0hamri <7mid7imri@gmail.com>
2023-12-07 10:41:59 +0100

    script 0

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -19,7 +19,8 @@ size_t print_list(const list_t *h)
 	{
 		if (!h->str)
 			printf("[0] (nil)\n");
-		printf("[%d] %s\n", h->len, h->str);
+		else
+			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
 		i++;
 	}
