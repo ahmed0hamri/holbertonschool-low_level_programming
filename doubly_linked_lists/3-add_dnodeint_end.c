df28518b3cea35bd02aebba09cbcdf2b331e604d
ahmed0hamri <7mid7imri@gmail.com>
2023-12-11 11:38:08 +0100

    script3

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -7,17 +7,17 @@
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-    dlistint_t *new_node, *tp, = malloc(sizeof(dlistint_t));
+    dlistint_t *new_node, *tp
+                              new_node = malloc(sizeof(dlistint_t));
     if (!new_node)
         return (NULL);
-    new_node->prev = NULL;
-    new_node->n= n;
+    new_node->n = n;
     new_node->next = NULL
-        *tp = *head;
-    while (*tp->next != NULL)
-        *tp = *tp->next;
-    *tp->next = new_node;
-    new_node->prev = *tp;
-    return new_node;
-}
 
+        while (tp && tp->next)
+            tp = *tp->next;
+    tp = tp->next;
+    tp->next = new_node;
+    new_node->prev = tp;
+    return (new_node);
+}
