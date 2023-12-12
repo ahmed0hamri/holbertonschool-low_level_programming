575478f2f9273a9fe5790f93ff0d2ccecc22c689
ahmed0hamri <7mid7imri.com>
2023-12-12 13:00:54 -0800

    script 0

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * read_textfile -  a function that reads a text file and prints it
+ * @filename: name of file (char)
+ * @letters: size_t
+ * Return: 0 or n
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int filo, texto, txt;
+	char *texto;
+	filo = open(filename, O_RDONLY);
+	if (filo == -1)
+		return 0;
+	texto = malloc(sizeof(char *letters));
+	if (letters == 0)
+		txt = read(filo, texto, letters);
+	write(STDOUT_FILENO, texto, txt);
+	free(texto);
+	close(texto);
+	return (txt);
+}
