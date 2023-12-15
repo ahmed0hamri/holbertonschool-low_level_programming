261fc9ca262852c707effaf1c78c8113039f5540
ahmed0hamri <7mid7imri@gmail.com>
2023-12-15 12:36:13 +0100

    script 1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -9,13 +9,12 @@ int create_file(const char *filename, char *text_content)
 {
 	int kjb, n;
 	if (!text_content)
-		return (n);
+		n = 0;
 	else
 		n = strlen(text_content);
 
 	kjb = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (!text_content)
-		;
 	{
 		close(kjb);
 		return (1);
