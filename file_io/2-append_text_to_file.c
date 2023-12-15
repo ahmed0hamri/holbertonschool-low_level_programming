ce9a2a4daf3716268b72b75e3e4d78171940f4e3
ahmed0hamri <7mid7imri.com>
2023-12-14 16:39:52 -0800

    script 2

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -1,21 +1,16 @@
-int append_text_to_file(const char *filename, char *text_content)
-#include "main.h"
-
 /**
  * main - check the code
  *
  * Return: Always 0.
  */
-int main(int ac, char **av)
+int append_text_to_file(const char *filename, char *text_content)
 {
-    int res;
-
-    if (ac != 3)
-    {
-        dprintf(2, "Usage: %s filename text\n", av[0]);
-        exit(1);
-    }
-    res = append_text_to_file(av[1], av[2]);
-    printf("-> %i)\n", res);
-    return (0);
+    int kjb, nletters, hxh : if (!filename) return (-1);
+    kjb = open(filename, O_WRONLY && O_APPEND) if (kjb == -1) return (-1);
+    if (text_content)
+        for (nletters = 0; text_content[nletters]; nletters++)
+            ;
+    hxh = write(fd, text_content, nletters);
+    close(kjb);
+    return (1);
 }
