aa069979d5387198beebbc4f9a7f8568d2d771b3
ahmed0hamri <7mid7imri.com>
2023-12-14 16:28:54 -0800

    script 2

---
+++ file_io/2-append_text_to_file.c
@@ -0,0 +1,21 @@
+int append_text_to_file(const char *filename, char *text_content)
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(int ac, char **av)
+{
+    int res;
+
+    if (ac != 3)
+    {
+        dprintf(2, "Usage: %s filename text\n", av[0]);
+        exit(1);
+    }
+    res = append_text_to_file(av[1], av[2]);
+    printf("-> %i)\n", res);
+    return (0);
+}
