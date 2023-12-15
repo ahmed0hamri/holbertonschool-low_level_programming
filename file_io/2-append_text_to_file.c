7875ceacd8fcbc32dda742e366a2071dd603e18b
ahmed0hamri <7mid7imri@gmail.com>
2023-12-15 12:47:38 +0100

    script 2

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -6,16 +6,16 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-    int kjb, nletters, hxh;
-     if (!filename) 
-     return (-1);
-    kjb = open(filename, O_WRONLY && O_APPEND) 
-    if (kjb == -1) 
-    return (-1);
-    if (!text_content)
-        return(-1);
-            
-    hxh = write(fd, text_content, nletters);
-    close(kjb);
-    return (1);
+	int kjb, nletters, hxh;
+	if (!filename)
+		return (-1);
+	kjb = open(filename, O_WRONLY && O_APPEND);
+	if (kjb == -1)
+		return (-1);
+	if (!text_content)
+		return (-1);
+
+	hxh = write(fd, text_content, nletters);
+	close(kjb);
+	return (1);
 }
