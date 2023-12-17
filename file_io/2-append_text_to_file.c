80e6a8169523bc1e3eba441c4977f4f071f10987
ahmed0hamri <7mid7imri.com>
2023-12-17 19:11:20 +0100

    script 2

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -12,16 +12,15 @@ int append_text_to_file(const char *filename, char *text_content)
 		nletters = 0;
 	else
 		(nletters = strlen(text_content));
-	kjb = open(filename, O_CREAT | O_WRONLY | O_APPEND,0600);
+	kjb = open(filename, O_CREAT | O_WRONLY | O_APPEND, 0600);
 	if (kjb == -1)
 		return (-1);
 	if (!text_content)
 	{
 		close(kjb);
-		return(1);
-
+		return (1);
 	}
-	
+
 	write(kjb, text_content, nletters);
 	close(kjb);
 	return (1);
